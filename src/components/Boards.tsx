5b38af4e43dd4392b72e6413512a3fbfef409f18
kshitish <kshitish182@gmail.com>
2020-12-06 16:35:19 +0545

    Add required dependencies and set up initial folder structure

---
+++ src/components/Boards.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Boards = () => <div>Hello Board</div>;
+
+export default Boards;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import Boards from './components/Boards';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Boards />
   </React.StrictMode>,
   document.getElementById('root')
 );
